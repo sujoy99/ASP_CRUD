cfe9af00d304964d60869da64d79fef289c59e14
scg.sujoy <scg.sujoy@gmail.com>
2020-07-03 10:29:25 +0600

    commited 1

---
+++ ASP_CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_CRUD.Startup))]
+namespace ASP_CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
